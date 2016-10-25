f4c3bcd1179fabe69d55a9e9563efa6b74f3dbf8
Jacob Mischka <jacobmischka@gmail.com>
2016-10-25 15:46:02 -0500

    Add some headers

--- src/server.js
+++ src/server.js
@@ -24,7 +24,11 @@ app.get('/combined.ics', (req, res) => {
 		}));
 	}
 
+	res.set('Expires', 'Mon, 01 Jan 1990 00:00:00 GMT');
+	res.set('Date', new Date().toGMTString());
 	res.set('Content-Type', 'text/calendar; charset=UTF-8');
+	res.set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+	res.set('Pragma', 'no-cache');
 
 	Promise.all(promises).then(() => {
 		res.send(merge(inputs));
