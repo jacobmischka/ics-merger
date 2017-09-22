4a768027f1ae04b42717e7bd9df84da64dfa5ddb
Jacob Mischka <jacob@mischka.me>
2017-09-22 08:09:34 -0500

    Add subGroups before grouped calendars

--- src/utils.js
+++ src/utils.js
@@ -204,17 +204,7 @@ export function getCalendars(
 
 		calendar = allCalendarGroups[calendarId];
 
-		if (calendar.calendars) {
-			let calIds = calendar.calendars;
-			let cals = calIds.map(id => allCalendars[id]);
-			calendars.push(...cals);
-			eventSources.push(...cals.map(cal => getSource(cal)));
-			for (let calId of calIds) {
-				if (!(calId in calendarMap))
-					calendarMap[calId] = allCalendars[calId];
-			}
-		}
-
+		// Add subGroups first
 		if (calendar.subGroups) {
 			let subGroupIds = calendar.subGroups.filter(id =>
 				id in allCalendarGroups && allCalendarGroups[id] !== calendar
@@ -241,6 +231,17 @@ export function getCalendars(
 				eventSources.push(...deepCalendars
 					.map(cal => getSource(cal, subGroup.color))
 				);
+			}
+		}
+
+		if (calendar.calendars) {
+			let calIds = calendar.calendars;
+			let cals = calIds.map(id => allCalendars[id]);
+			calendars.push(...cals);
+			eventSources.push(...cals.map(cal => getSource(cal)));
+			for (let calId of calIds) {
+				if (!(calId in calendarMap))
+					calendarMap[calId] = allCalendars[calId];
 			}
 		}
 	}
